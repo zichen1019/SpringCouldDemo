9a5f596b76e5203a82d4217d2b41334f8af8946e
zichen1019 <1510748736@qq.com>
2020-09-08 17:26:33 +0800

    重新提交一次：移除类名：Class clazz

--- zc-common/src/main/java/com/zc/common/config/enums/ConvertEnum.java
+++ zc-common/src/main/java/com/zc/common/config/enums/ConvertEnum.java
@@ -13,20 +13,15 @@ public enum ConvertEnum {
     /**
      * 转换对饮枚举
      */
-    STRING(String.class, "toStr"),
-    INTEGER(Integer.class, "toInt"),
-    LONG(Long.class, "toLong"),
-    FLOAT(Float.class, "toFloat"),
-    DOUBLE(Double.class, "toDouble"),
-    DATE(Date.class, "toDate"),
-    BOOLEAN(Boolean.class, "toBool"),
-    BIGDECIMAL(BigDecimal.class, "toBigDecimal"),
-    NONE(null, "");
-
-    /**
-     * 类名
-     */
-    private Class clazz;
+    STRING("toStr"),
+    INTEGER("toInt"),
+    LONG("toLong"),
+    FLOAT("toFloat"),
+    DOUBLE("toDouble"),
+    DATE("toDate"),
+    BOOLEAN("toBool"),
+    BIGDECIMAL("toBigDecimal"),
+    NONE(null);
 
     /**
      * 方法名
